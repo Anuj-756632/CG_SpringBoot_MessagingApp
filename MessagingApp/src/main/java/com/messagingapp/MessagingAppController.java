456ae24af4378072d6d25fc2964cdd0e13b4c7da
Anuj-756632 <anujvishwakarma302004@gmail.com>
2025-02-27 10:25:18 +0530

    [Anuj] Add. Make Rest call Program

--- MessagingApp/src/main/java/com/messagingapp/MessagingAppController.java
+++ MessagingApp/src/main/java/com/messagingapp/MessagingAppController.java
@@ -2,6 +2,7 @@ package com.messagingapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,12 @@ public class MessagingAppController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+
+    //Use Case 2
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(name = "name", defaultValue = "Mark") String name) {
+        return "Hello, " + name + " from BridgeLabz!";
     }
 }
