a4bba81c822cfde625d47141dd3e064d6e808c16
Anuj-756632 <anujvishwakarma302004@gmail.com>
2025-02-27 12:18:14 +0530

    [Anuj] Add. Make Rest Call to Create UserDTO Bean

--- MessagingApp/src/main/java/com/messagingapp/UserDTO.java
+++ MessagingApp/src/main/java/com/messagingapp/UserDTO.java
@@ -1,4 +1,32 @@
 package com.messagingapp;
 
 public class UserDTO {
+        private String firstName;
+        private String lastName;
+
+        // Default constructor
+        public UserDTO() {}
+
+        // Constructor with parameters
+        public UserDTO(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        // Getters and Setters
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
 }
